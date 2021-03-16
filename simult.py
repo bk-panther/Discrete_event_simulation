20d92d8042cf87371108d1942746f2ca92f6769a
bk-panther <priyeshjain@cse.iitb.ac.in>
2021-03-16 14:46:08 +0530

    first commit

---
+++ simult.py
@@ -0,0 +1,30 @@
+import numpy as np
+class Simulation:
+    def __init__(self, users):
+        self.clock = 0.0
+        self.client = Client(users)
+        
+
+    def advance_time():
+        pass
+    
+
+class Client:
+    def __init__(self, users):
+        self.no_of_users = users
+        self.user = []
+        for i in range(10):
+            self.user.append(Users())
+        print(user)
+
+    def generate_thinktime():
+        pass
+    def generate_timeout():
+        pass
+
+
+class Users():
+    pass
+
+s = Simulation(10)
+# print(s.client.user)
